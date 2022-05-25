090810951eb9cfc93d2800012969f648b407d5b3
Nontakorn Konkain <nonami798@gmail.com>
2022-05-25 23:07:06 +0700

    UI networkHelper

---
+++ UI_NetworkHelper/NetworkHelper.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace TestCallAPI
+{
+    public class NetworkHelper
+    {
+        private string host = "http://localhost:8000/";
+        public List<Dictionary<string, string>> getData(string url)
+        {
+            List<Dictionary<string, string>> data = getMethod(url).Result;
+            
+            return data ?? null; 
+        }
+        private async Task<List<Dictionary<string, string>>> getMethod(string url)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(host);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                // Above three lines can be replaced with new helper method below
+                //var data = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(responseBody);
+                //Console.WriteLine(responseBody);
+                //return responseBody ?? null;
+                var data = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(responseBody);
+                return data;
+
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+                return null ;
+            }
+        }
+
+        public List<Dictionary<string, string>> postData(string url, object ob)
+        {
+            List<Dictionary<string, string>> data = postMethod(url, ob).Result;
+
+            return data ?? null;
+        }
+        private async Task<List<Dictionary<string, string>>> postMethod(string url, object ob)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(host);
+                client.DefaultRequestHeaders.Accept.Clear();
+                if(ob != null)
+                {
+                    var json = JsonConvert.SerializeObject(ob);
+                    var dataJson = new StringContent(json, Encoding.UTF8, "application/json");
+
+                    var response = await client.PostAsync(url, dataJson);
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    var data = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(responseBody);
+                    return data;
+                }
+                else
+                {
+                    return null;
+                }
+
+            }
+            catch(HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ UI_NetworkHelper/Users.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCallAPI
+{
+    public class Users
+    {
+        public int ID { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string name { get; set; }
+        public string last_name { get; set; }
+        public string address { get; set; }
+    }
+}
