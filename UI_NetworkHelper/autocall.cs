7ba81a032ca2fdafb8a2fb9081466ce8dd2d8886
Nontakorn Konkain <nonami798@gmail.com>
2022-06-15 22:22:06 +0700

    add auto call api

---
+++ UI_NetworkHelper/autocall.cs
@@ -0,0 +1,34 @@
+using System.Threading;
+
+public Form1()
+        {
+            InitializeComponent();
+            Thread trd = new Thread(new ThreadStart(this.getValue));
+            trd.IsBackground = true;
+            trd.Start();
+        }
+
+        private void getValue()
+        {
+
+            string url = "/hw/get_by_id?ID=1";
+            string temp = "";
+            while (true)
+            {
+                var data = con.getData(url);
+                if (data != null)
+                {
+                    string x = data[0]["value"].ToString();
+                    temp = x;
+                    label1.Invoke((MethodInvoker)(() => label1.Text = x));
+                    textBox1.Invoke((MethodInvoker)(() => textBox1.Text = x));
+                }
+                else
+                {
+                    label1.Invoke((MethodInvoker)(() => label1.Text = temp));
+                }
+
+                
+                Thread.Sleep(500);
+            }
+        } 
