ad00e91d584915ec1eb2998a531c69fd89824f94
Nontakorn Konkain <nonami798@gmail.com>
2022-06-16 21:55:41 +0700

    fixbug

--- UI_NetworkHelper/NetworkHelper.cs
+++ UI_NetworkHelper/NetworkHelper.cs
@@ -8,82 +8,113 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using Newtonsoft.Json;
 
-namespace WindowsFormsApp1
+namespace miniproject
 {
     //version 2.0.1 fixbug timeout
     public class NetworkHelper
     {
-        private string host = "http://18.141.11.24";
-        public List<Dictionary<string, string>> getData(string url)
+        public static string host { get; set; }
+         
+        public NetworkHelper(string h)
         {
-            List<Dictionary<string, string>> data = getMethod(url).Result;
-            
-            return data ?? null; 
-        }
-        private async Task<List<Dictionary<string, string>>> getMethod(string url)
-        {
-            try
+            if(h != "")
             {
-                HttpClient client = new HttpClient();
-                client.BaseAddress = new Uri(host);
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                HttpResponseMessage response = await client.GetAsync(url).ConfigureAwait(false);
-                response.EnsureSuccessStatusCode();
-                string responseBody = await response.Content.ReadAsStringAsync();
-                // Above three lines can be replaced with new helper method below
-                //var data = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(responseBody);
-                //Console.WriteLine(responseBody);
-                //return responseBody ?? null;
-                var data = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(responseBody);
-                return data;
-
+               NetworkHelper.host = h;
             }
-            catch (HttpRequestException e)
-            {
-                Console.WriteLine("\nException Caught!");
-                Console.WriteLine("Message :{0} ", e.Message);
-                return null ;
-            }
+           
         }
-
-        public List<Dictionary<string, string>> postData(string url, object ob)
-        {
-            List<Dictionary<string, string>> data = postMethod(url, ob).Result;
-
-            return data ?? null;
-        }
-        private async Task<List<Dictionary<string, string>>> postMethod(string url, object ob)
-        {
-            try
+            public List<Dictionary<string, string>> getData(string path, string param)
             {
-                HttpClient client = new HttpClient();
-                client.BaseAddress = new Uri(host);
-                client.DefaultRequestHeaders.Accept.Clear();
-                if(ob != null)
+                string url = "";
+                if (param != "")
                 {
-                    var json = JsonConvert.SerializeObject(ob);
-                    var dataJson = new StringContent(json, Encoding.UTF8, "application/json");
+                    url = string.Format($"{path}?{param}");
+                }
+                else {
+                    url = path;
 
-                    var response = await client.PostAsync(url, dataJson).ConfigureAwait(false);
+                }
+                List<Dictionary<string, string>> data = getMethod(url).Result;
+
+                return data ?? null;
+            }
+            private async Task<List<Dictionary<string, string>>> getMethod(string url)
+            {
+            string host = "http://"+NetworkHelper.host;
+                try
+                {
+                    HttpClient client = new HttpClient();
+                    client.BaseAddress = new Uri(host);
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    HttpResponseMessage response = await client.GetAsync(url).ConfigureAwait(false);
                     response.EnsureSuccessStatusCode();
                     string responseBody = await response.Content.ReadAsStringAsync();
-
+                    // Above three lines can be replaced with new helper method below
+                    //var data = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(responseBody);
+                    //Console.WriteLine(responseBody);
+                    //return responseBody ?? null;
                     var data = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(responseBody);
                     return data;
+
                 }
-                else
+                catch (HttpRequestException e)
                 {
+                    Console.WriteLine("\nException Caught!");
+                    Console.WriteLine($"Message :{e.Message} ");
                     return null;
                 }
-
             }
-            catch(HttpRequestException e)
+
+            public List<Dictionary<string, string>> postData(string path, string param ,object ob)
             {
-                Console.WriteLine("\nException Caught!");
-                Console.WriteLine("Message :{0} ", e.Message);
-                return null;
+                string url = "";
+                if (param != "")
+                {
+                    url = string.Format($"{path}?{param}");
+                }
+                else {
+                    url = path;
+
+                }
+
+                List<Dictionary<string, string>> data = postMethod(url, ob).Result;
+
+                return data ?? null;
+            }
+            private async Task<List<Dictionary<string, string>>> postMethod(string url, object ob)
+            {
+            string host = "http://" + NetworkHelper.host;
+            try
+                {
+                    HttpClient client = new HttpClient();
+                    client.BaseAddress = new Uri(host);
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    if (ob != null)
+                    {
+                        var json = JsonConvert.SerializeObject(ob);
+                        var dataJson = new StringContent(json, Encoding.UTF8, "application/json");
+
+                        var response = await client.PostAsync(url, dataJson).ConfigureAwait(false);
+                        response.EnsureSuccessStatusCode();
+                        string responseBody = await response.Content.ReadAsStringAsync();
+
+                        var data = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(responseBody);
+                        return data;
+                    }
+                    else
+                    {
+                        return null;
+                    }
+
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine("\nException Caught!");
+                    Console.WriteLine($"Message :{e.Message} ");
+                    return null;
+                }
             }
         }
     }
-}
+
